f2cb5e29d0566ecd11cab8037b94ee096a71127c
Nnocj1 <nicholascommey2001@gmail.com>
2024-01-31 15:04:06 +0000

    change position

--- Practice/w03-practice/w03-practice.js
+++ Practice/w03-practice/w03-practice.js
@@ -9,3 +9,38 @@ const arrHTML = originalArr.map(function (arr) {return `<li>${arr}</li>`});
 
 //Last but not least is to place the new array in the HTML document where it's needed.
 document.getElementById("myList").innerHTML = arrHTML.join();
+
+const letterGrade = ['A','B','A'];
+
+function gpaGrades(x) {
+    if (x == 'A')
+    {
+       gpa = 4;
+    }
+
+    else if (x == 'B')
+    {
+        gpa = 3;
+    }
+
+    else if (x == 'C')
+    {
+        gpa = 2;
+    }
+
+    else if (x == 'D')
+    {
+        gpa = 1;
+    }
+
+    else if (x == 'E'|| x == 'F')
+    {
+        gpa = 0;
+    }
+
+    else 
+    {
+        gpa = 0;
+    }
+
+}
