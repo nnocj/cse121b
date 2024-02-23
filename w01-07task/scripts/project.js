3ab1323cec2e1b14af46e46feffee0fbbf0a3043
Nnocj1 <nicholascommey2001@gmail.com>
2024-02-23 07:22:44 +0000

    final project

---
+++ w01-07task/scripts/project.js
@@ -0,0 +1,35 @@
+/* Project  */
+
+/* Step 1: Declare and initialize global variables */
+const postElement = document.querySelector(".adventurePost");
+let templeList = [];
+
+/* Step 2: async displayPosts Function */
+const displayPosts = async (posts) => {
+    posts.forEach(post => {
+        const figure = document.createElement("figure");
+        
+        const h3 = document.createElement("h3");
+        h3.textContent = temple.templeName;
+        
+        const img = document.createElement("img");
+        img.src = post.image;
+        img.alt = post.title;
+        
+        figure.appendChild(img);
+       
+        
+        postsElement.appendChild(figure);
+    });
+};
+
+/* Step 3: async getPosts Function using fetch()*/
+const getPosts = async () => {
+    const response = await fetch("https://byui-cse.github.io/cse121b-ww-course/resources/temples.json");
+    postList = await response.json();
+    displayPosts(postList);
+};
+
+
+// Call getTemples function to start the application
+getPosts();
